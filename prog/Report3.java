8e86437545ee35424812a64ed52596ed5fcd56d0
eiji_miyazawa <b1800722@planet.kanazawa-it.ac.jp>
2019-06-16 18:40:04 +0900

    レポート用のプログラム完成

--- prog/Report3.java
+++ prog/Report3.java
@@ -1,18 +1,42 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
-class MyThrad extends Thread{
-    //int theradId;
+class MyThread implements Runnable {
     public void run(){
         Random random = new Random();
-        System.out.printf("%d",random.nextInt(9) + 2);
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        try{
+            String startTime = dateFormatter.format(LocalTime.now());
+
+            long s = random.nextInt(10) + 1;
+            Thread.sleep(1000 * s);
+
+            String endTime = dateFormatter.format(LocalTime.now());
+            System.out.printf("%3d start %s  wait %2d  end %s\n", Thread.currentThread().getId(), startTime, s, endTime);
+        }catch(InterruptedException e){
+            System.out.println(e);
+        }
     }
 }
 
 public class Report3{
     public static void main(String[] args) {
-        MyThrad t[] = new MyThrad[3];
-        for(int i = 0; i < t.length; i++){
-            t[i].start();
+        MyThread t = new MyThread();
+        Thread[] threads = new Thread[30];
+        for(int i = 0; i < threads.length; i++){
+            threads[i] = new Thread(t);
+            threads[i].start();
         }
+
+        for(int i = 0; i < threads.length; i++){
+            try{
+                threads[i].join();
+            }catch(InterruptedException e){
+                System.out.println(e);
+            }
+        }
+        System.out.println("Program end");
     }
 }
