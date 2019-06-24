a0ba7f93cd59aa27a6f937ce4e2b418e153e2887
eijiMiyazawa <b1800722@planet.kanazawa-it.ac.jp>
2019-06-24 13:20:35 +0900

    プログラム修正

--- prog/Report3.java
+++ prog/Report3.java
@@ -2,34 +2,49 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
-class MyThread implements Runnable {
+class MyThread implements Runnable{
     public void run(){
+        //interaction wait Second
         Random random = new Random();
+        long wait = random.nextInt(9) + 2;
+        
+        //Get Thread ID
         long id = Thread.currentThread().getId();
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-        String startTime = dateFormatter.format(LocalTime.now());
 
+        //Get Start Time
+        DateTimeFormatter dF = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String startTime = dF.format(LocalTime.now());
+
+        //sleep
         try{
-            long s = random.nextInt(9) + 2;
-            Thread.sleep(1000 * s);
-
-            String endTime = dateFormatter.format(LocalTime.now());
-            System.out.printf("%2d start %s  wait %2d  end %s\n", id, startTime, s, endTime);
+            Thread.sleep(1000 * wait);
         }catch(InterruptedException e){
             System.out.println(e);
         }
+
+        //Get End Time
+        String endTime = dF.format(LocalTime.now());
+
+        //print
+        System.out.printf("%d start %s wait %2d end %s\n", id, startTime, wait, endTime);
     }
 }
 
 public class Report3{
     public static void main(String[] args) {
+        //thread creation
         MyThread t = new MyThread();
         Thread[] threads = new Thread[30];
+        for(int i = 0; i < threads.length; i++){
+            threads[i] = new Thread(t);
+        }
 
-        for(int i = 0; i < threads.length; i++) threads[i] = new Thread(t);
+        //thread start
+        for(int i = 0; i < threads.length; i++){
+            threads[i].start();
+        }
 
-        for(int i = 0; i < threads.length; i++) threads[i].start();
-
+        //thread observation
         for(int i = 0; i < threads.length; i++){
             try{
                 threads[i].join();
@@ -37,6 +52,7 @@ public class Report3{
                 System.out.println(e);
             }
         }
+
         System.out.println("Program end");
     }
 }
